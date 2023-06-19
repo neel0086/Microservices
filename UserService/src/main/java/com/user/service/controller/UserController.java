5b10e70b1b5c70fffdd23426f2d8d3649f170e3f
Neel Mehta <neelmehta0086@gmail.com>
2023-06-20 00:00:45 +0530

    feat:UserService backend completed

--- UserService/src/main/java/com/user/service/controller/UserController.java
+++ UserService/src/main/java/com/user/service/controller/UserController.java
@@ -1,13 +1,33 @@
 package com.user.service.controller;
 
 import com.user.service.entity.User;
+import com.user.service.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User user1 = userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user1);
+    }
 
-public interface UserController {
-    User saveUser(User user);
+    @GetMapping("/{id}")
+    public ResponseEntity<User> createUser(@PathVariable Long id){
+        User user1 = userService.getUser(id);
+        return ResponseEntity.ok(user1);
+    }
 
-    List<User> getAllUser();
-
-    User getUser(int id);
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUser(){
+        List<User> user1 = userService.getAllUser();
+        return ResponseEntity.ok(user1);
+    }
 }
--- UserService/src/main/java/com/user/service/services/UserServiceImpl.java
+++ UserService/src/main/java/com/user/service/services/UserServiceImpl.java
@@ -4,9 +4,11 @@ import com.user.service.entity.User;
 import com.user.service.exception.ResourceNotFoundException;
 import com.user.service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
