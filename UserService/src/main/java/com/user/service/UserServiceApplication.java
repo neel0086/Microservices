e7dd4e28363772c0896e53145863daa92c278751
Neel Mehta <neelmehta0086@gmail.com>
2023-06-21 23:42:18 +0530

    feat:Connecting user with rating service

--- UserService/src/main/java/com/user/service/UserServiceApplication.java
+++ UserService/src/main/java/com/user/service/UserServiceApplication.java
@@ -3,10 +3,14 @@ package com.user.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class UserServiceApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
--- UserService/src/main/java/com/user/service/services/UserServiceImpl.java
+++ UserService/src/main/java/com/user/service/services/UserServiceImpl.java
@@ -3,14 +3,23 @@ package com.user.service.services;
 import com.user.service.entity.User;
 import com.user.service.exception.ResourceNotFoundException;
 import com.user.service.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    private Logger logger = LoggerFactory.getLogger(UserService.class);
     @Autowired
     private UserRepository userRepository;
     @Override
@@ -25,6 +34,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(Long userId) {
-        return userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("Given user not found"));
+        User user = userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("Given user not found"));
+        ArrayList forObject =  restTemplate.getForObject("");
+        return user;
     }
 }
