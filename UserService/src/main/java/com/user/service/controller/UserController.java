7452d41718fa711544c2c1cc6710054c50d50c86
Neel Mehta <neelmehta0086@gmail.com>
2023-06-26 23:14:26 +0530

    feat:Resilience4j

--- UserService/src/main/java/com/user/service/controller/UserController.java
+++ UserService/src/main/java/com/user/service/controller/UserController.java
@@ -2,6 +2,7 @@ package com.user.service.controller;
 
 import com.user.service.entity.User;
 import com.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,9 +21,20 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
+    @CircuitBreaker(name="hotelRatingBreaker",fallbackMethod = "hotelRatingFallback")
     public ResponseEntity<User> createUser(@PathVariable Long id){
         User user1 = userService.getUser(id);
         return ResponseEntity.ok(user1);
+    }
+
+    public ResponseEntity<User> hotelRatingFallback(Long userId,Exception ex){
+//        logger.info("Fallback is executed because service is down",ex.getMessage());
+        User user  =  User.builder()
+                .email("unknown@gmail.com")
+                .name("unknown")
+                .about("Service is down")
+                .userId(Long.valueOf(123)).build();
+        return new ResponseEntity<>(user,HttpStatus.OK);
     }
 
     @GetMapping
